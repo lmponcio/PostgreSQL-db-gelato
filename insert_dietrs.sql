c90ac67c6750e5de2b98a095d5416b3ba17e41ab
lmponcio <lmponcio@gmail.com>
2023-06-20 14:12:19 +1000

    insert queries added

---
+++ insert_dietrs.sql
@@ -0,0 +1,25 @@
+INSERT INTO dietr VALUES (
+    1,
+    'vegan',
+    'no animal products or byproducts'
+    );
+INSERT INTO dietr VALUES (
+    2,
+    'alcohol_free',
+    'without any alcohol content or presence'
+    );
+INSERT INTO dietr VALUES (
+    3,
+    'egg_free',
+    'not containing eggs or egg-based ingredients'
+    );
+INSERT INTO dietr VALUES (
+    4,
+    'gluten_free',
+    'not containing gluten'
+    );
+INSERT INTO dietr VALUES (
+    5,
+    'nut_free',
+    'not containing nuts or nut-based ingredients'
+    );
