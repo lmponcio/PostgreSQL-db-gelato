7ab7c7a0d9d1224ff6ea06325d2c4bc037cd0760
lmponcio <lmponcio@gmail.com>
2023-06-26 19:16:39 +1000

    add insert sql statements

---
+++ insert_icecream_dietrs.sql
@@ -0,0 +1,557 @@
+
+INSERT INTO icecream_dietr VALUES (
+                '1',
+                '1'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '1',
+                '2'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '1',
+                '3'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '1',
+                '4'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '1',
+                '5'
+                );
+
+INSERT INTO icecream_dietr VALUES (
+                '2',
+                '2'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '2',
+                '3'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '2',
+                '4'
+                );
+
+INSERT INTO icecream_dietr VALUES (
+                '3',
+                '2'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '3',
+                '3'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '3',
+                '4'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '3',
+                '5'
+                );
+
+INSERT INTO icecream_dietr VALUES (
+                '4',
+                '2'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '4',
+                '3'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '4',
+                '4'
+                );
+
+INSERT INTO icecream_dietr VALUES (
+                '5',
+                '2'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '5',
+                '3'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '5',
+                '4'
+                );
+
+INSERT INTO icecream_dietr VALUES (
+                '6',
+                '2'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '6',
+                '3'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '6',
+                '4'
+                );
+
+INSERT INTO icecream_dietr VALUES (
+                '7',
+                '1'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '7',
+                '2'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '7',
+                '3'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '7',
+                '4'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '7',
+                '5'
+                );
+
+INSERT INTO icecream_dietr VALUES (
+                '8',
+                '2'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '8',
+                '3'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '8',
+                '4'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '8',
+                '5'
+                );
+
+INSERT INTO icecream_dietr VALUES (
+                '9',
+                '2'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '9',
+                '3'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '9',
+                '4'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '9',
+                '5'
+                );
+
+INSERT INTO icecream_dietr VALUES (
+                '10',
+                '2'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '10',
+                '3'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '10',
+                '5'
+                );
+
+INSERT INTO icecream_dietr VALUES (
+                '11',
+                '2'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '11',
+                '3'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '11',
+                '4'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '11',
+                '5'
+                );
+
+INSERT INTO icecream_dietr VALUES (
+                '12',
+                '2'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '12',
+                '3'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '12',
+                '4'
+                );
+
+INSERT INTO icecream_dietr VALUES (
+                '13',
+                '2'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '13',
+                '3'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '13',
+                '4'
+                );
+
+INSERT INTO icecream_dietr VALUES (
+                '14',
+                '2'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '14',
+                '3'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '14',
+                '4'
+                );
+
+INSERT INTO icecream_dietr VALUES (
+                '15',
+                '4'
+                );
+
+INSERT INTO icecream_dietr VALUES (
+                '16',
+                '1'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '16',
+                '2'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '16',
+                '3'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '16',
+                '4'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '16',
+                '5'
+                );
+
+INSERT INTO icecream_dietr VALUES (
+                '17',
+                '2'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '17',
+                '3'
+                );
+
+INSERT INTO icecream_dietr VALUES (
+                '18',
+                '1'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '18',
+                '2'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '18',
+                '3'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '18',
+                '4'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '18',
+                '5'
+                );
+
+INSERT INTO icecream_dietr VALUES (
+                '19',
+                '2'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '19',
+                '3'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '19',
+                '4'
+                );
+
+INSERT INTO icecream_dietr VALUES (
+                '20',
+                '2'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '20',
+                '3'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '20',
+                '4'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '20',
+                '5'
+                );
+
+INSERT INTO icecream_dietr VALUES (
+                '21',
+                '1'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '21',
+                '2'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '21',
+                '3'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '21',
+                '4'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '21',
+                '5'
+                );
+
+
+INSERT INTO icecream_dietr VALUES (
+                '23',
+                '1'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '23',
+                '2'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '23',
+                '3'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '23',
+                '4'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '23',
+                '5'
+                );
+
+INSERT INTO icecream_dietr VALUES (
+                '24',
+                '2'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '24',
+                '3'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '24',
+                '4'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '24',
+                '5'
+                );
+
+INSERT INTO icecream_dietr VALUES (
+                '25',
+                '2'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '25',
+                '3'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '25',
+                '4'
+                );
+
+INSERT INTO icecream_dietr VALUES (
+                '26',
+                '2'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '26',
+                '3'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '26',
+                '4'
+                );
+
+INSERT INTO icecream_dietr VALUES (
+                '27',
+                '1'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '27',
+                '2'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '27',
+                '3'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '27',
+                '4'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '27',
+                '5'
+                );
+
+INSERT INTO icecream_dietr VALUES (
+                '28',
+                '3'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '28',
+                '4'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '28',
+                '5'
+                );
+
+INSERT INTO icecream_dietr VALUES (
+                '29',
+                '2'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '29',
+                '3'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '29',
+                '4'
+                );
+
+INSERT INTO icecream_dietr VALUES (
+                '30',
+                '1'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '30',
+                '2'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '30',
+                '3'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '30',
+                '4'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '30',
+                '5'
+                );
+
+INSERT INTO icecream_dietr VALUES (
+                '31',
+                '2'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '31',
+                '3'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '31',
+                '4'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '31',
+                '5'
+                );
+
+INSERT INTO icecream_dietr VALUES (
+                '32',
+                '1'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '32',
+                '2'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '32',
+                '3'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '32',
+                '4'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '32',
+                '5'
+                );
+
+INSERT INTO icecream_dietr VALUES (
+                '33',
+                '2'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '33',
+                '3'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '33',
+                '4'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '33',
+                '5'
+                );
+
+INSERT INTO icecream_dietr VALUES (
+                '34',
+                '5'
+                );
+
+INSERT INTO icecream_dietr VALUES (
+                '35',
+                '2'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '35',
+                '4'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '35',
+                '5'
+                );
+
+INSERT INTO icecream_dietr VALUES (
+                '36',
+                '2'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '36',
+                '3'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '36',
+                '4'
+                );
+
+INSERT INTO icecream_dietr VALUES (
+                '37',
+                '2'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '37',
+                '3'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '37',
+                '4'
+                );
+INSERT INTO icecream_dietr VALUES (
+                '37',
+                '5'
+                );
---
+++ insert_icecreams.sql
@@ -0,0 +1,190 @@
+INSERT INTO icecream VALUES (
+        1,
+        'Blood Orange Sorbet',
+        'Made with the Redbelly Citrus blood oranges from Griffiths, NSW.'
+        );
+INSERT INTO icecream VALUES (
+        2,
+        'Bounty',
+        'Coconut milk gelato with desiccated coconut and choc chip.'
+        );
+INSERT INTO icecream VALUES (
+        3,
+        'Boysenberry',
+        'Our own yoghurt gelato with poached boysenberry coulis.'
+        );
+INSERT INTO icecream VALUES (
+        4,
+        'Chocolate Chip',
+        'Light cocoa gelato with chocolate chips (all Messina chocolate is made in-house)'
+        );
+INSERT INTO icecream VALUES (
+        5,
+        'Chocolate Fondant',
+        'Dietary Information: ﻿Dairy, Nuts'
+        );
+INSERT INTO icecream VALUES (
+        6,
+        'Chocolate Mint',
+        'Dietary Information: ﻿Dairy, Nuts'
+        );
+INSERT INTO icecream VALUES (
+        7,
+        'Chocolate Sorbet',
+        '
+
+A rich decadent dark chocolate sorbet.
+
+Dietary Information: Vegan
+'
+        );
+INSERT INTO icecream VALUES (
+        8,
+        'Coconut & Lychee',
+        'Coconut milk gelato with lychee fruit - this was the first ever flavour we made from scratch.'
+        );
+INSERT INTO icecream VALUES (
+        9,
+        'Coffee',
+        'Made with real espresso coffee (seriously).'
+        );
+INSERT INTO icecream VALUES (
+        10,
+        'Cookies & Cream',
+        'Light cocoa gelato with crumbled cookies. Only available at selected stores.'
+        );
+INSERT INTO icecream VALUES (
+        11,
+        'Dulce De Leche',
+        'Argentinean caramelised milk - we make our own dulce de leche in-house.'
+        );
+INSERT INTO icecream VALUES (
+        12,
+        'Gianduia',
+        'Chocolate & hazelnut gelato. Think Nutella... but better.'
+        );
+INSERT INTO icecream VALUES (
+        13,
+        'Hazelnut',
+        'Dietary Information: ﻿Dairy, Nuts'
+        );
+INSERT INTO icecream VALUES (
+        14,
+        'Hokey Pokey',
+        'Dietary Information: ﻿Dairy, Nuts'
+        );
+INSERT INTO icecream VALUES (
+        15,
+        'Italian Nougat',
+        'Dietary Information: ﻿Alcohol, Dairy, Egg, Nuts'
+        );
+INSERT INTO icecream VALUES (
+        16,
+        'Lemon Sorbet',
+        'Lemon sorbet made with damn fresh Eureka lemons and candied lemon zest.'
+        );
+INSERT INTO icecream VALUES (
+        17,
+        'Macadamia Crunch',
+        'Caramelised white chocolate and macadamia gelato with macadamia crunch.'
+        );
+INSERT INTO icecream VALUES (
+        18,
+        'Mango Sorbet',
+        'Mango sorbet made from Laudham Park mangos in Townsville, QLD.'
+        );
+INSERT INTO icecream VALUES (
+        19,
+        'Milk Choc with Choc Peanut Fudge',
+        'Dietary Information: ﻿Dairy, Nuts'
+        );
+INSERT INTO icecream VALUES (
+        20,
+        'Nicky Glasses',
+        'Dietary Information: ﻿Dairy'
+        );
+INSERT INTO icecream VALUES (
+        21,
+        'Pandan & Coconut Sorbet',
+        'Pandan & Coconut sorbet. Pandan is a green asian leaf used in Asian cooking.'
+        );
+INSERT INTO icecream VALUES (
+        22,
+        'Panna Cotta with Fig Jam and Amaretti Biscuit',
+        'Dietary Information: ﻿Alcohol, Dairy, Egg, Nuts, Gluten'
+        );
+INSERT INTO icecream VALUES (
+        23,
+        'Passionfruit Sorbet',
+        'Passionfruit sorbet made from really good, real passionfruits.'
+        );
+INSERT INTO icecream VALUES (
+        24,
+        'Pear & Rhubarb',
+        'Pear gelato with our own poached rhubarb.'
+        );
+INSERT INTO icecream VALUES (
+        25,
+        'Pistachio',
+        'Made with genuine Sicilian pistachios from Bronte, Italy.'
+        );
+INSERT INTO icecream VALUES (
+        26,
+        'Pistachio Praline',
+        'Fior di latte gelato with white chocolate and pistachio fudge and pistachio praline.'
+        );
+INSERT INTO icecream VALUES (
+        27,
+        'Raspberry Sorbet',
+        'Rasberry sorbet. Got some bloody good raspberries in it.'
+        );
+INSERT INTO icecream VALUES (
+        28,
+        'Rum & Raisin',
+        'Dietary Information: Alcohol, Dairy'
+        );
+INSERT INTO icecream VALUES (
+        29,
+        'Salted Caramel and White Chocolate',
+        'Dietary Information: ﻿Dairy, Nuts'
+        );
+INSERT INTO icecream VALUES (
+        30,
+        'Salted Coconut & Mango Sorbet',
+        'None'
+        );
+INSERT INTO icecream VALUES (
+        31,
+        'Strawberries & Cream',
+        'Strawberry and vanilla gelato with strawberry jam. Strawberries are from our farm in Dural, NSW.'
+        );
+INSERT INTO icecream VALUES (
+        32,
+        'Strawberry Sorbet',
+        'Made from our very own strawberries from our strawberry farm in Dural, NSW.'
+        );
+INSERT INTO icecream VALUES (
+        33,
+        'Super Dulce De Leche',
+        'Dulce de leche gelato with dulce de leche throughout. We make our own dulce de leche in-house.'
+        );
+INSERT INTO icecream VALUES (
+        34,
+        'Tiramisu',
+        'Dietary Information: ﻿Alcohol, Dairy, Egg, Gluten'
+        );
+INSERT INTO icecream VALUES (
+        35,
+        'Vanilla',
+        'Made using organic Heilala Tahitian vanilla bean from Tonga, free range eggs and our own fresh Jersey milk,'
+        );
+INSERT INTO icecream VALUES (
+        36,
+        'White Chocolate Hazelnut',
+        'Dietary Information: ﻿Dairy, Nuts'
+        );
+INSERT INTO icecream VALUES (
+        37,
+        'Yoghurt & Caramel',
+        'Yoghurt gelato smothered dulce de leche - made with our own yoghurt and dulce de leche.'
+        );
