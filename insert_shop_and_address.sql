992b8864f81b17c79c8030f511048eb51496f0c8
lmponcio <lmponcio@gmail.com>
2023-06-26 22:54:27 +1000

    add shop and address data

---
+++ insert_shop_and_address.sql
@@ -0,0 +1,13 @@
+INSERT INTO shop VALUES (
+    1,
+    'Practice SQL Shop',
+    'Shop where dietary requirements are respected',
+    '0000000000'
+    );
+INSERT INTO address VALUES (
+    1,
+    '42',
+    'Wallaby Way',
+    'Sydney',
+    1
+    );
