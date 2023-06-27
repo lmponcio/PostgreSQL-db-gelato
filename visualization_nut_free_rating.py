bf6c5c1ad929d7737d0c9642308b6b61f2924e94
Martin Poncio <89647104+lmponcio@users.noreply.github.com>
2023-06-27 21:16:12 +1000

    Update visualization_nut_free_rating.py

--- visualization_nut_free_rating.py
+++ visualization_nut_free_rating.py
@@ -5,18 +5,14 @@ data = pd.read_csv("average_nut_free.csv")
 # reversing order for cosmetic purposes
 data = data.iloc[::-1]
 
-# Prepare data for plotting
 labels = data["name"]
 values = data["average_rating"]
 
-# Create the horizontal bar plot
 plt.barh(labels, values)
 
-# Customize the plot
 plt.xlabel("Average Review Rating")
 plt.ylabel("Gelato Name")
 plt.title("Nut Free Gelato Average Ratings")
 plt.tight_layout()
 
-# Display the plot
 plt.show()
